d668adce12d2d60ff3986482791bcda8265fed8a
gerzarko <germanzarkovich@gmail.com>
2026-02-06 17:50:42 -0800

    add changes to run throw the x and y of the window

--- code_editor.c
+++ code_editor.c
@@ -1,9 +1,9 @@
 #include "sys/ioctl.h"
+#include <asm-generic/ioctls.h>
 #include <stdint.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
-
-struct winsize windowSize;
 
 int main(int argc, char **argv) {
 
@@ -11,11 +11,18 @@ int main(int argc, char **argv) {
     return 1;
   }
 
+  // struct que tiene el tamano de la pantalla en pixeles
+  struct winsize windowSize;
+  if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &windowSize) != 0) {
+    return 1;
+  }
+
   printf("\033[?47h");
   printf("\033[0;0f");
   fflush(stdout);
 
-  char byteBuffer[4096];
+  uint32_t sizeOfbuffer = sizeof(windowSize.ws_row * windowSize.ws_col);
+  char *byteBuffer = malloc(sizeOfbuffer);
 
   FILE *fd = fopen(argv[1], "r");
   if (fd == NULL) {
@@ -25,9 +32,17 @@ int main(int argc, char **argv) {
 
   // fd es el puntero al inicio del archivo, bytebuffer es la canatidad de bytes
   // que vamos leyendo de ese archivo
-  uint32_t itemsRead = fread(byteBuffer, 1, 4096, fd);
+  uint32_t itemsRead = fread(byteBuffer, 1, sizeOfbuffer, fd);
   if (itemsRead == 0) {
     return 1;
+  }
+
+  for (uint32_t y = 0; y < windowSize.ws_row; y++) {
+    for (uint32_t x = 0; x < windowSize.ws_col; x++) {
+      printf("\033[%d;%df", y, x);
+      fflush(stdout);
+      printf("%c", byteBuffer[x + (y * windowSize.ws_col)]);
+    }
   }
 
   printf("%s", byteBuffer);
@@ -35,6 +50,6 @@ int main(int argc, char **argv) {
   fclose(fd);
   // sleep(5);
   getchar();
-  printf("\033[?47l");
+  printf("\033[?1049l");
   fflush(stdout);
 }
