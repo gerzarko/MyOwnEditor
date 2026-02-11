9a355dae003127a262d95dc8ec9e9d98092c179f
gerzarko <germanzarkovich@gmail.com>
2026-02-10 17:16:49 -0800

    add refactor but it's broken now

--- Justfile
+++ Justfile
@@ -1,4 +1,3 @@
 run: 
     ./compile.sh ; ./out/editor testfile.txt  log.txt
-  
 
--- code_editor.c
+++ code_editor.c
@@ -3,6 +3,7 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 
 typedef struct State {
@@ -15,11 +16,14 @@ typedef struct State {
 
 } State;
 
+void DrawToScreen(State state, FILE *log);
+
 int main(int argc, char **argv) {
 
   // if (argc != 2) {
   //   return 1;
   // }
+  //
 
   // struct que tiene el tamano de la pantalla en pixeles
   struct winsize windowSize;
@@ -28,45 +32,59 @@ int main(int argc, char **argv) {
   }
 
   printf("\033[?1049h");
+  // printf("\033[H");
   printf("\033[0;0f");
   // printf("\033[H");
   fflush(stdout);
 
-  uint32_t sizeOfbuffer = windowSize.ws_row * windowSize.ws_col;
-  char *byteBuffer = malloc(sizeOfbuffer);
-
-  FILE *fd = fopen(argv[1], "r");
-  if (fd == NULL) {
-    return 1;
-  }
-
+  // Declare State struct
+  State state = {};
+  state.bufferSize = windowSize.ws_col * windowSize.ws_row;
+  state.byteBuffer = malloc(state.bufferSize);
+  state.colAmount = windowSize.ws_col;
+  state.rowAmount = windowSize.ws_row;
+  state.fd = fopen(argv[1], "r");
   FILE *log = fopen(argv[2], "w");
-  if (log == NULL) {
-    return 1;
-  }
-  // fd es el puntero al inicio del archivo, bytebuffer es la canatidad de bytes
-  // que vamos leyendo de ese archivo
-  uint32_t itemsRead = fread(byteBuffer, 1, sizeOfbuffer, fd);
-  if (itemsRead == 0) {
-    return 1;
+  if (state.fd == NULL) {
+    return 10;
   }
 
-  fclose(fd);
+  int input = 0;
 
-  getchar();
+  do {
+    uint32_t itemsRead = fread(state.byteBuffer, 1, state.bufferSize, state.fd);
+    if (itemsRead == 0) {
+      char logLine[4096];
+      sprintf(logLine, "el germancito estuvo aqui\n");
+      fwrite(logLine, strlen(logLine), 1, log);
+      printf("aca");
+      // state.byteBuffer = state.byteBuffer + 200;
+    }
+    DrawToScreen(state, log);
+    input = getchar();
+    char logLine[4096];
+    sprintf(logLine, "%d\n", input);
+    fwrite(logLine, strlen(logLine), 1, log);
+  } while (input != 133);
+
+  // fd es el puntero al inicio del archivo, bytebuffer es la canatidad de
+  // bytes que vamos leyendo de ese archivo
+  fclose(state.fd);
+
   printf("\033[?1049l");
   fflush(stdout);
 }
 
-void PrintToScreen(State state, FILE *log) {
+void DrawToScreen(State state, FILE *log) {
   uint32_t i = 0;
-  for (uint32_t y = 1; y < state.rowAmount; y++) {
-    for (uint32_t x = 1; x < state.colAmount; x++) {
+
+  for (uint32_t y = 0; y < state.rowAmount; y++) {
+    for (uint32_t x = 0; x < state.colAmount; x++) {
       if (state.byteBuffer[i] == '\n') {
         i++;
         break;
       }
-      printf("\033[%d;%df", y, x);
+      printf("\033[%d;%df", y + 1, x + 1);
       fflush(stdout);
       printf("%c", state.byteBuffer[i]);
       i++;
